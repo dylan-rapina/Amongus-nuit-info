6a07ec574ebf7f39810604870e3c8b1c0a679200
DylanR <rapinadylan@outlook.fr>
2020-12-04 00:21:49 +0100

    onavancebcp

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/youwon', (req, res) => res.render('pages/youwon'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   //.use(favicon(path.join(__dirname,'public','images','IconeWeb.PNG')));
